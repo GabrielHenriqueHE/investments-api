8b11fdf1fccd1583a5b3eafb77c94b6d163c8fa6
Gabriel Evaristo <gabriel.evaristo.2004@gmail.com>
2025-03-15 18:01:36 -0300

    feat: initial commit

---
+++ src/test/java/io/github/gabrielhenriquehe/investment_api/InvestmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrielhenriquehe.investment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
